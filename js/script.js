055f2f0cb9f11a17e3997eb9fcb44e2883cd2e59
Ashish Raj Singh <ashishrajsingh75@gmail.com>
2026-03-12 10:51:10 +0530

    javascript directory

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// document.writeln("Hello World");
+
+{
+       let a = 1;
+      console.log(a);
+}
+
+let a =3;
+console.log(a/2);
+
+/*
+Symbol
+String
+Null 
+Number
+Boolean
+BigInt
+Undefined
+
+*/
